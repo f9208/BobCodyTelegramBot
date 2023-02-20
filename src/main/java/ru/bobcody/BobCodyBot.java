a77d9d3a120a3e62d693ea825fd5b0d1e2a02261
Mikryukov Ivan <f9208@ya.ru>
2023-02-20 23:29:10 +0300

    Убрал закомментированный код

--- src/main/java/ru/bobcody/BobCodyBot.java
+++ src/main/java/ru/bobcody/BobCodyBot.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.bobcody.services.SettingService;
 
@@ -18,23 +19,10 @@ public class BobCodyBot extends TelegramWebhookBot {
         super(settingService.getBotToken());
         this.settingService = settingService;
     }
-//    @Autowired
-//    private BotFacade botFacade;
 
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-//        try {
-//            System.out.println(update);
-//            if (update.getMessage() != null) {
-//                System.out.println(update.getMessage().getText());
-//            }
-//            botFacade.handleUpdate(update);
-//            return null;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return new SendMessage(update.getMessage().getChatId().toString(), SMTH_GET_WRONG_BROKEN);
-//        }
-        return null;
+        return new SendMessage();
     }
 
     @Override
